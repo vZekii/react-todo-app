2bc3e36a19cd5075ae90f0354308ddb13dc526c7
zekii <zach7505@gmail.com>
2021-06-06 18:41:46 +1000

    finished todo app

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react'
 import './App.css';
 
+function useListState (initalValue: string[]) {
+  const [list, setList] = useState(initalValue);
+
+  return {
+    list,
+    
+    addFunc: (todo: string) => {
+      if (todo.length > 0) {
+        setList([...list, todo])
+      }
+    },
+
+    delFunc: (position: number) => {
+      const newList = list.filter((_, index) => index !== position);
+      setList(newList);
+    }
+  };
+};
+
+const List = (props: { items: string[]; delFunc: (index: number) => void}) => (
+  <ul id="mylist">
+    {props.items.map((text: string, i: number) => 
+          <li key={i}>
+            {text}
+            <input type="button" value="❌" onClick={e => props.delFunc(i)}/>
+          </li>
+      )}
+  </ul>
+)
+
+const Form = (props: {addFunc: (todo: string) => void}) => {
+  const [value, setValue] = useState("");
+  
+  const handleSubmit = (evt: { preventDefault: () => void; }) => {
+      evt.preventDefault();
+      props.addFunc(value);
+      setValue("");
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Todo Item:
+        <input
+          type="text"
+          value={value}
+          onChange={e => setValue(e.target.value)}
+        />
+      </label>
+      <input type="submit" value="Submit" />
+    </form>
+  );
+}
+
 function App() {
+  const {list, addFunc, delFunc} = useListState([]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Form addFunc={addFunc} />
+      <List items={list} delFunc={delFunc}/>
+
     </div>
   );
 }
